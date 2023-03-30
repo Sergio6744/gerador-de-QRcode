3e9b868bc0f399284aba18cde299bddbb09b13ee
Sérgio <101438156+Sergio6744@users.noreply.github.com>
2023-03-30 20:41:02 -0300

    alguumas adiçoes
    adicionamdo favicon e algumas correçoes

--- js.js
+++ js.js
@@ -1,12 +1,15 @@
 let baixar = document.querySelector("#baixar")
 let recarregar = document.querySelector("#recarregar")
 let QR = document.querySelector("#QR")
+let erro = document.querySelector("h2")
 
 function limpar() {
     let url_pesq = document.querySelector("#url")
     QR.style.display = "none"
     QR.src = undefined
     url_pesq.value = ""
+    baixar.style.background = "rgb(186,186,186)"
+    recarregar.style.background = "rgb(186,186,186)"
 }
 
 document.addEventListener("keyup", ()=>{
@@ -15,6 +18,8 @@ document.addEventListener("keyup", ()=>{
         QR.style.display = "block"
         let api = `https://chart.googleapis.com/chart?cht=qr&chs=300x300&chl=${url}`
         QR.src = api
+        baixar.style.background = "white"
+        recarregar.style.background = "white"
     } if(url.length === 0) {
         limpar()
     }
@@ -32,11 +37,15 @@ baixar.addEventListener("click", ()=>{
         fetch(`https://chart.googleapis.com/chart?cht=qr&chs=300x300&chl=${url}`)
         .then(resp => resp.blob())
         .then((resp)=>{
+            erro.style.display = "none"
             a.href = URL.createObjectURL(resp)
             a.download = "qr.png"
             document.body.appendChild(a)
             a.click()
             URL.revokeObjectURL(a.href)
+        })
+        .catch(()=>{
+            erro.style.display = "block"
         })
     }
 })
